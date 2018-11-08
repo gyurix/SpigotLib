7f0877a57d6b27a1b4da467d9de0080359d3383d
gyurix <barathgy@gmail.com>
2018-11-08 18:59:18 +0100

    Version 8.4 added to plugin.yml and Main class so I won't forget about it when I release the update

--- src/main/java/gyurix/spigotlib/Main.java
+++ src/main/java/gyurix/spigotlib/Main.java
@@ -84,7 +84,7 @@ public class Main extends JavaPlugin implements Listener {
     /**
      * Current version of the plugin, stored here to not be able to be abused so easily by server owners, by changing the plugin.yml file
      */
-    public static final String version = "8.1";
+    public static final String version = "8.4";
     /**
      * Data directory of the plugin (plugins/SpigotLib folder)
      */
