89c22b6ea47dd6a178752b21739303180faea940
gyurix <barathgy@gmail.com>
2019-01-29 18:57:11 +0100

    Added missing default use entity action = interact

--- src/main/java/gyurix/protocol/wrappers/inpackets/PacketPlayInUseEntity.java
+++ src/main/java/gyurix/protocol/wrappers/inpackets/PacketPlayInUseEntity.java
@@ -13,43 +13,43 @@ import gyurix.spigotutils.ServerVersion;
 import java.lang.reflect.Method;
 
 public class PacketPlayInUseEntity extends WrappedPacket {
-    public EntityUseAction action;
-    public int entityId;
-    public HandType hand;
-    public Vector targetLocation;
+  public EntityUseAction action;
+  public int entityId;
+  public HandType hand;
+  public Vector targetLocation;
+
+  @Override
+  public Object getVanillaPacket() {
+    return PacketInType.UseEntity.newPacket(entityId, action.toNMS(), targetLocation == null ? null : targetLocation.toNMS(), hand == null ? null : hand.toNMS());
+  }
+
+  @Override
+  public void loadVanillaPacket(Object packet) {
+    Object[] d = PacketInType.UseEntity.getPacketData(packet);
+    entityId = (int) d[0];
+    action = d[1] == null ? EntityUseAction.INTERACT : EntityUseAction.valueOf(d[1].toString());
+    if (Reflection.ver.isAbove(ServerVersion.v1_8))
+      targetLocation = d[2] == null ? null : new Vector(d[2]);
+    if (Reflection.ver.isAbove(ServerVersion.v1_9))
+      hand = d[3] == null ? null : HandType.valueOf(d[3].toString());
+  }
+
+  public enum EntityUseAction implements WrappedData {
+    INTERACT,
+    ATTACK,
+    INTERACT_AT;
+    Method valueOf = Reflection.getMethod(Reflection.getNMSClass("PacketPlayInUseEntity$EnumEntityUseAction"), "valueOf", String.class);
 
     @Override
-    public Object getVanillaPacket() {
-        return PacketInType.UseEntity.newPacket(entityId, action.toNMS(), targetLocation == null ? null : targetLocation.toNMS(), hand == null ? null : hand.toNMS());
+    public Object toNMS() {
+      try {
+        return valueOf.invoke(null, name());
+      } catch (Throwable e) {
+        SU.error(SU.cs, e, "SpigotLib", "gyurix");
+      }
+      return null;
     }
 
-    @Override
-    public void loadVanillaPacket(Object packet) {
-        Object[] d = PacketInType.UseEntity.getPacketData(packet);
-        entityId = (int) d[0];
-        action = EntityUseAction.valueOf(d[1].toString());
-        if (Reflection.ver.isAbove(ServerVersion.v1_8))
-            targetLocation = d[2] == null ? null : new Vector(d[2]);
-        if (Reflection.ver.isAbove(ServerVersion.v1_9))
-            hand = d[3] == null ? null : HandType.valueOf(d[3].toString());
-    }
-
-    public enum EntityUseAction implements WrappedData {
-        INTERACT,
-        ATTACK,
-        INTERACT_AT;
-        Method valueOf = Reflection.getMethod(Reflection.getNMSClass("PacketPlayInUseEntity$EnumEntityUseAction"), "valueOf", String.class);
-
-        @Override
-        public Object toNMS() {
-            try {
-                return valueOf.invoke(null, name());
-            } catch (Throwable e) {
-                SU.error(SU.cs, e, "SpigotLib", "gyurix");
-            }
-            return null;
-        }
-
-    }
+  }
 }
 
