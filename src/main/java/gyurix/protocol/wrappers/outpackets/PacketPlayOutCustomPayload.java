56a652715acf6160cd32700f83816f1695e56cb9
Gyurix <barathgy@gmail.com>
2019-04-09 01:06:41 +0200

    Added custom payload 1.7 support

--- src/main/java/gyurix/protocol/wrappers/outpackets/PacketPlayOutCustomPayload.java
+++ src/main/java/gyurix/protocol/wrappers/outpackets/PacketPlayOutCustomPayload.java
@@ -3,6 +3,7 @@ package gyurix.protocol.wrappers.outpackets;
 import gyurix.protocol.event.PacketOutType;
 import gyurix.protocol.wrappers.WrappedPacket;
 import gyurix.spigotlib.SU;
+import gyurix.spigotutils.ServerVersion;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 
@@ -15,38 +16,44 @@ import static gyurix.protocol.Reflection.*;
  * Created by gyurix on 25/03/2017.
  */
 public class PacketPlayOutCustomPayload extends WrappedPacket {
-    private static final Constructor con = getConstructor(getNMSClass("PacketDataSerializer"), ByteBuf.class);
-    private static final Field f = getField(getNMSClass("PacketDataSerializer"), "a");
-    public String channel;
-    public byte[] data;
+  private static final Constructor con = getConstructor(getNMSClass("PacketDataSerializer"), ByteBuf.class);
+  private static final Field f = getField(getNMSClass("PacketDataSerializer"), "a");
+  public String channel;
+  public byte[] data;
 
-    public PacketPlayOutCustomPayload() {
+  public PacketPlayOutCustomPayload() {
 
+  }
+
+  public PacketPlayOutCustomPayload(String channel, byte[] data) {
+    this.channel = channel;
+    this.data = data;
+  }
+
+  @Override
+  public Object getVanillaPacket() {
+    try {
+      if (ver.isAbove(ServerVersion.v1_8))
+        return PacketOutType.CustomPayload.newPacket(channel, con.newInstance(ByteBufAllocator.DEFAULT.buffer().writeBytes(data)));
+      else
+        return PacketOutType.CustomPayload.newPacket(channel, data);
+    } catch (Throwable e) {
+      SU.error(SU.cs, e, "SpigotLib", "gyurix");
     }
+    return null;
+  }
 
-    public PacketPlayOutCustomPayload(String channel, byte[] data) {
-        this.channel = channel;
-        this.data = data;
+  @Override
+  public void loadVanillaPacket(Object obj) {
+    Object[] data = PacketOutType.CustomPayload.getPacketData(obj);
+    channel = (String) data[0];
+    try {
+      if (ver.isAbove(ServerVersion.v1_8))
+        this.data = ((ByteBuf) f.get(data[1])).array();
+      else
+        this.data = (byte[]) data[1];
+    } catch (Throwable e) {
+      SU.error(SU.cs, e, "SpigotLib", "gyurix");
     }
-
-    @Override
-    public Object getVanillaPacket() {
-        try {
-            return PacketOutType.CustomPayload.newPacket(channel, con.newInstance(ByteBufAllocator.DEFAULT.buffer().writeBytes(data)));
-        } catch (Throwable e) {
-            SU.error(SU.cs, e, "SpigotLib", "gyurix");
-        }
-        return null;
-    }
-
-    @Override
-    public void loadVanillaPacket(Object obj) {
-        Object[] data = PacketOutType.CustomPayload.getPacketData(obj);
-        channel = (String) data[0];
-        try {
-            this.data = ((ByteBuf) f.get(data[1])).array();
-        } catch (Throwable e) {
-            SU.error(SU.cs, e, "SpigotLib", "gyurix");
-        }
-    }
+  }
 }
