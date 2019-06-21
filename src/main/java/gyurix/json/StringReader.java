454e737d8a316364fe211e2d19104c1186862572
Gyurix <barathgy@gmail.com>
2019-06-21 18:51:24 +0200

    Re-added StringReader

---
+++ src/main/java/gyurix/json/StringReader.java
@@ -0,0 +1,23 @@
+package gyurix.json;
+
+public class StringReader {
+  public int id;
+  public char[] str;
+
+  public StringReader(String in) {
+    str = in.toCharArray();
+  }
+
+  public boolean hasNext() {
+    return id < str.length;
+  }
+
+  public char last() {
+    return str[id - 1];
+  }
+
+  public char next() {
+    return str[id++];
+  }
+}
+
