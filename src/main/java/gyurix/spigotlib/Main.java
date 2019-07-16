1cc12242ffb856fe4ce5f6144e4b4ae50e20f91d
Gyurix <barathgy@gmail.com>
2019-07-16 11:24:11 +0200

    9.0.2 version mark

--- src/main/java/gyurix/spigotlib/Main.java
+++ src/main/java/gyurix/spigotlib/Main.java
@@ -86,7 +86,7 @@ public class Main extends JavaPlugin implements Listener {
   /**
    * Current version of the plugin, stored here to not be able to be abused so easily by server owners, by changing the plugin.yml file
    */
-  public static final String version = "9.0.1";
+  public static final String version = "9.0.2";
   /**
    * Data directory of the plugin (plugins/SpigotLib folder)
    */
