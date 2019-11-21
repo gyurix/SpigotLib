dcf627f52e5d98750b270c6a8923d8e7885705aa
Baráth György <barathgy@gmail.com>
2019-11-21 20:41:09 +0100

    Added custom Gui decorative items

--- src/main/java/gyurix/inventory/GUIConfig.java
+++ src/main/java/gyurix/inventory/GUIConfig.java
@@ -2,13 +2,15 @@ package gyurix.inventory;
 
 import gyurix.spigotlib.SU;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
 
 import static gyurix.spigotutils.ItemUtils.fillVariables;
 
 public class GUIConfig {
+  public HashMap<Integer, ItemStack> items = new HashMap<>();
   public ItemStack separator;
   public int size;
   public String title;
@@ -16,10 +18,10 @@ public class GUIConfig {
   public Inventory getInventory(int size, CustomGUI gui, Object... vars) {
     size = (size + 8) / 9 * 9;
     Inventory inv = Bukkit.createInventory(gui, size, SU.fillVariables(title, vars));
-    if (separator != null && separator.getType() != Material.AIR) {
-      ItemStack sep = fillVariables(separator, vars);
-      for (int i = 0; i < size; i++)
-        inv.setItem(i, sep);
+    ItemStack sep = fillVariables(separator, vars);
+    for (int i = 0; i < size; i++) {
+      ItemStack is = items.get(i);
+      inv.setItem(i, is == null ? sep : fillVariables(is, vars));
     }
     return inv;
   }
